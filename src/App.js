e418b1a7c5d0c5c5046648748fb7982a7f1335b9
ApolloSolo <matt1teixeira@gmail.com>
2022-05-22 13:41:46 -0400

    Dynamically render movies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
-import { render } from "@testing-library/react";
-import React from "react";
+import { useEffect, useState } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+
+const API_URL = "http://www.omdbapi.com/?apikey=377eff31";
+
 
 const App = () => {
-  return <h1>App</h1>;
+    const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search)
+  };
+
+  useEffect(() => {
+    searchMovies("The Lord of the Rings");
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Movies-R-Us</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
+
+      {
+          movies.length > 0
+          ? (
+            <div className="container">
+                {movies.map((movie) => (
+                    <MovieCard movie={movie}/>
+                ))}
+            </div>
+          ) : (
+              <div className="empty">
+                  <h2>No Movies Found</h2>
+              </div>
+          )
+      }
+
+
+    </div>
+  );
 };
 
 export default App;
